89cf73667bd9a701cda226c29d5ef7b5ec25b039
pranaymethuku <pranaymethuku@gmail.com>
2020-04-25 22:25:14 -0400

    Code formatting

--- research/object_detection/inference_scripts/gui_styled.py
+++ research/object_detection/inference_scripts/gui_styled.py
@@ -23,12 +23,14 @@ from PIL import Image
 
 VIDEOS = [".mov", ".mp4", ".flv", ".avi", ".ogg", ".wmv"]
 
+
 class Ui_MainWindow(QWidget):
     def setupUi(self, MainWindow):
         # Initial setup of the GUI
         MainWindow.setObjectName("MainWindow")
         #MainWindow.resize(978, 748)
-        MainWindow.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowMinimizeButtonHint)
+        MainWindow.setWindowFlags(
+            Qt.WindowCloseButtonHint | Qt.WindowMinimizeButtonHint)
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
         font.setPointSize(24)
@@ -41,7 +43,8 @@ class Ui_MainWindow(QWidget):
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.horizontalLayoutWidget.setGeometry(QtCore.QRect(10, 0, 961, 51))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
-        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(
+            self.horizontalLayoutWidget)
         self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.label = QtWidgets.QLabel(self.horizontalLayoutWidget)
@@ -60,7 +63,8 @@ class Ui_MainWindow(QWidget):
 
         # Set-up for the steps (right below the title)
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.formLayoutWidget)
-        self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
+        self.horizontalLayout.setSizeConstraint(
+            QtWidgets.QLayout.SetDefaultConstraint)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setObjectName("horizontalLayout")
 
@@ -75,21 +79,26 @@ class Ui_MainWindow(QWidget):
         self.horizontalLayout.addWidget(self.step1Label)
 
         # Creates the drop-down menu for the tiers
-        self.step1ChooseTierComboBox = QtWidgets.QComboBox(self.formLayoutWidget)
+        self.step1ChooseTierComboBox = QtWidgets.QComboBox(
+            self.formLayoutWidget)
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
         font.setPointSize(12)
         self.step1ChooseTierComboBox.setFont(font)
         self.step1ChooseTierComboBox.setObjectName("step1ChooseTierComboBox")
-        self.step1ChooseTierComboBox.addItems(["Tier 1", "Tier 2", "Tier 3", "Tier 4"])
-        self.step1ChooseTierComboBox.setStyleSheet("background-color: #e8e9eb; font: black")
+        self.step1ChooseTierComboBox.addItems(
+            ["Tier 1", "Tier 2", "Tier 3", "Tier 4"])
+        self.step1ChooseTierComboBox.setStyleSheet(
+            "background-color: #e8e9eb; font: black")
         self.horizontalLayout.addWidget(self.step1ChooseTierComboBox)
 
         # Based on what tier is selected, models will be placed
-        self.step1ChooseTierComboBox.currentIndexChanged[str].connect(self.on_tier_currentIndexChanged)
+        self.step1ChooseTierComboBox.currentIndexChanged[str].connect(
+            self.on_tier_currentIndexChanged)
 
         # Add spacers for GUI to look cleaner
-        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        spacerItem = QtWidgets.QSpacerItem(
+            40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
 
         # Creates and styles the step 2 title
@@ -103,27 +112,33 @@ class Ui_MainWindow(QWidget):
         self.horizontalLayout.addWidget(self.step2Label)
 
         # Creates the drop-down menu for the models
-        self.step2ChooseModelComboBox = QtWidgets.QComboBox(self.formLayoutWidget)
+        self.step2ChooseModelComboBox = QtWidgets.QComboBox(
+            self.formLayoutWidget)
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
         font.setPointSize(12)
         self.step2ChooseModelComboBox.setFont(font)
         self.step2ChooseModelComboBox.setObjectName("step2ChooseModelComboBox")
-        self.step2ChooseModelComboBox.addItems(['Faster RCNN Inception V2 Coco', 'SSD Inception V2 Coco'])
-        self.step2ChooseModelComboBox.setStyleSheet("border: 1px solid black; background-color: #e8e9eb")
+        self.step2ChooseModelComboBox.addItems(
+            ['Faster RCNN Inception V2 Coco', 'SSD Inception V2 Coco'])
+        self.step2ChooseModelComboBox.setStyleSheet(
+            "border: 1px solid black; background-color: #e8e9eb")
         self.horizontalLayout.addWidget(self.step2ChooseModelComboBox)
 
-        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        spacerItem1 = QtWidgets.QSpacerItem(
+            40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem1)
 
         # Creates and styles the step 3 title
-        self.step3UploadImageOrVideoLabel = QtWidgets.QLabel(self.formLayoutWidget)
+        self.step3UploadImageOrVideoLabel = QtWidgets.QLabel(
+            self.formLayoutWidget)
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
         font.setPointSize(14)
         font.setUnderline(True)
         self.step3UploadImageOrVideoLabel.setFont(font)
-        self.step3UploadImageOrVideoLabel.setObjectName("step3UploadImageOrVideoLabel")
+        self.step3UploadImageOrVideoLabel.setObjectName(
+            "step3UploadImageOrVideoLabel")
         self.horizontalLayout.addWidget(self.step3UploadImageOrVideoLabel)
 
         # Creates the Upload button
@@ -133,7 +148,8 @@ class Ui_MainWindow(QWidget):
         font.setPointSize(12)
         self.upload_button.setFont(font)
         self.upload_button.setObjectName("upload_button")
-        self.upload_button.setStyleSheet("border: 1px solid black; background-color: #e8e9eb")
+        self.upload_button.setStyleSheet(
+            "border: 1px solid black; background-color: #e8e9eb")
         self.horizontalLayout.addWidget(self.upload_button)
         #self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         #self.pushButton.setGeometry(QtCore.QRect(870, 110, 101, 51))
@@ -146,7 +162,8 @@ class Ui_MainWindow(QWidget):
         font.setPointSize(12)
         self.capture_button.setFont(font)
         self.capture_button.setObjectName("capture_button")
-        self.capture_button.setStyleSheet("border: 1px solid black; background-color: #e8e9eb")
+        self.capture_button.setStyleSheet(
+            "border: 1px solid black; background-color: #e8e9eb")
         self.horizontalLayout.addWidget(self.capture_button)
         self.capture_button.clicked.connect(self.capture_media)
 
@@ -168,7 +185,8 @@ class Ui_MainWindow(QWidget):
         self.media_layout = QWidget(self.centralwidget)
         self.media_layout.setGeometry(QtCore.QRect(10, 140, 961, 491))
         self.media_layout.setObjectName("graphicsView")
-        self.media_layout.setStyleSheet("border: 2px solid black; background-color: #e8e9eb")
+        self.media_layout.setStyleSheet(
+            "border: 2px solid black; background-color: #e8e9eb")
 
         self.stop = QtWidgets.QPushButton(self.centralwidget)
         font = QtGui.QFont()
@@ -188,7 +206,8 @@ class Ui_MainWindow(QWidget):
         self.exit_button.setFont(font)
         self.exit_button.setGeometry(QtCore.QRect(860, 640, 113, 32))
         self.exit_button.setObjectName("exit_button")
-        self.exit_button.setStyleSheet("border: 1px solid black; background-color: #e8e9eb")
+        self.exit_button.setStyleSheet(
+            "border: 1px solid black; background-color: #e8e9eb")
         MainWindow.setCentralWidget(self.centralwidget)
         self.exit_button.clicked.connect(self.exit)
 
@@ -211,7 +230,6 @@ class Ui_MainWindow(QWidget):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
 
     def stop_webcam(self):
         self.timer.stop()
@@ -236,7 +254,8 @@ class Ui_MainWindow(QWidget):
 
         if file_extension[1] == ".jpg" or file_extension[1] == ".jpeg":
             # Run inference on image and display
-            detection.image_detection(frozen_graph, labelmap, name, "predicted.jpg")
+            detection.image_detection(
+                frozen_graph, labelmap, name, "predicted.jpg")
             self.display("predicted.jpg")
 
         if file_extension[1] in VIDEOS:
@@ -245,7 +264,8 @@ class Ui_MainWindow(QWidget):
                 self.media.itemAt(i).widget().deleteLater()
 
             # Run inference on video and display
-            detection.video_detection(frozen_graph, labelmap, name, os.getcwd() + os.path.sep + "predicted.mp4")
+            detection.video_detection(
+                frozen_graph, labelmap, name, os.getcwd() + os.path.sep + "predicted.mp4")
             self.display(os.getcwd() + os.path.sep + "predicted.mp4")
 
     def get_path(self):
@@ -254,13 +274,17 @@ class Ui_MainWindow(QWidget):
         labelmap = "../tor_results/tier_" + tier + "/labelmap.pbtxt"
         frozen_graph = ""
         if self.step2ChooseModelComboBox.currentText() == "Faster RCNN Inception V2 Coco":
-            frozen_graph = "../tor_results/tier_" + tier + "/faster_rcnn_inception_v2_coco_2018_01_28.pb"
+            frozen_graph = "../tor_results/tier_" + tier + \
+                "/faster_rcnn_inception_v2_coco_2018_01_28.pb"
         elif self.step2ChooseModelComboBox.currentText() == "Faster RCNN Resnet101 Kitti":
-            frozen_graph = "../tor_results/tier_" + tier + "/_faster_rcnn_resnet101_kitti_2018_01_28.pb"
+            frozen_graph = "../tor_results/tier_" + tier + \
+                "/_faster_rcnn_resnet101_kitti_2018_01_28.pb"
         elif self.step2ChooseModelComboBox.currentText() == "RFCN Resnet101 Coco":
-            frozen_graph = "../tor_results/tier_" + tier + "/rfcn_resnet101_coco_2018_01_28.pb"
+            frozen_graph = "../tor_results/tier_" + \
+                tier + "/rfcn_resnet101_coco_2018_01_28.pb"
         elif self.step2ChooseModelComboBox.currentText() == "SSD Inception V2 Coco":
-            frozen_graph = "../tor_results/tier_" + tier + "/ssd_inception_v2_coco_2018_01_28.tflite"
+            frozen_graph = "../tor_results/tier_" + tier + \
+                "/ssd_inception_v2_coco_2018_01_28.tflite"
         elif self.step2ChooseModelComboBox.currentText() == "SSD V2 Coco":
             frozen_graph = "../tor_results/tier_" + tier + "/ssd_v2_coco_2018_01_28.tflite"
         return labelmap, frozen_graph
@@ -297,7 +321,8 @@ class Ui_MainWindow(QWidget):
             self.player = QMediaPlayer()
             self.player.setVideoOutput(self.video)
             self.player.setMedia(QMediaContent(QUrl.fromLocalFile(media)))
-            self.player.setPosition(0)  # to start at the beginning of the video every time
+            # to start at the beginning of the video every time
+            self.player.setPosition(0)
 
             self.media.addWidget(self.video)
             self.player.play()
@@ -308,30 +333,31 @@ class Ui_MainWindow(QWidget):
 
         # Start webcam
         index = 0
-        self.capture=cv2.VideoCapture(index)
-        #self.image=self.capture.read()
-        #while self.image is None and index < 2: 
+        self.capture = cv2.VideoCapture(index)
+        # self.image=self.capture.read()
+        # while self.image is None and index < 2:
         #    index += 1
         #    self.capture=cv2.VideoCapture(index)
         #    self.image=self.capture.read()
-        
-        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 
         # Show stop button
         self.stop.setVisible(True)
 
-        self.timer=QTimer(self)
+        self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
         self.timer.start(5)
 
     def update_frame(self):
         # Get frame
-        _, self.image=self.capture.read()
+        _, self.image = self.capture.read()
         self.image = cv2.flip(self.image, 1)
 
         # Run inference on frame and display to screen
-        self.detected_image = detection.webcam_detection(self.frozen_graph, self.labelmap, True, self.image)
+        self.detected_image = detection.webcam_detection(
+            self.frozen_graph, self.labelmap, True, self.image)
         self.display_frame(self.detected_image)
 
     def display_frame(self, frame):
@@ -343,9 +369,10 @@ class Ui_MainWindow(QWidget):
             else:
                 qformat = QImage.Format_RGB888
 
-        outImage = QImage(frame, frame.shape[1], frame.shape[0], frame.strides[0], qformat)
+        outImage = QImage(
+            frame, frame.shape[1], frame.shape[0], frame.strides[0], qformat)
 
-        #BGR>>RGB
+        # BGR>>RGB
         outImage = outImage.rgbSwapped()
 
         pixmap = QPixmap.fromImage(outImage)
@@ -359,28 +386,36 @@ class Ui_MainWindow(QWidget):
         # Change the models to show based on tier selected
         self.step2ChooseModelComboBox.clear()
         if str(self.step1ChooseTierComboBox.currentText()) == 'Tier 1':
-            self.step2ChooseModelComboBox.addItems(['Faster RCNN Inception V2 Coco', 'SSD Inception V2 Coco'])
+            self.step2ChooseModelComboBox.addItems(
+                ['Faster RCNN Inception V2 Coco', 'SSD Inception V2 Coco'])
         elif str(self.step1ChooseTierComboBox.currentText()) == 'Tier 2':
             self.step2ChooseModelComboBox.addItems(['Faster RCNN Inception V2 Coco', 'Faster RCNN Resnet101 Kitti',
-            'RFCN Resnet101 Coco', 'SSD Inception V2 Coco'])
+                                                    'RFCN Resnet101 Coco', 'SSD Inception V2 Coco'])
         elif str(self.step1ChooseTierComboBox.currentText()) == 'Tier 3':
-            self.step2ChooseModelComboBox.addItems(['Faster RCNN Inception V2 Coco', 'SSD V2 Coco'])
+            self.step2ChooseModelComboBox.addItems(
+                ['Faster RCNN Inception V2 Coco', 'SSD V2 Coco'])
         else:
             self.step2ChooseModelComboBox.addItems(['SSD V2 Coco'])
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.showMaximized()
-        MainWindow.setWindowTitle(_translate("Video and Image Detection", "Video and Image Detection"))
-        self.label.setText(_translate("MainWindow", "Tiered Object Recognition - Image and Video Detection"))
-        self.step1Label.setText(_translate("MainWindow", "Step 1: Choose tier!"))
-        self.step2Label.setText(_translate("MainWindow", "Step 2: Choose model!"))
-        self.step3UploadImageOrVideoLabel.setText(_translate("MainWindow", "Step 3: Upload or capture!"))
+        MainWindow.setWindowTitle(_translate(
+            "Video and Image Detection", "Video and Image Detection"))
+        self.label.setText(_translate(
+            "MainWindow", "Tiered Object Recognition - Image and Video Detection"))
+        self.step1Label.setText(_translate(
+            "MainWindow", "Step 1: Choose tier!"))
+        self.step2Label.setText(_translate(
+            "MainWindow", "Step 2: Choose model!"))
+        self.step3UploadImageOrVideoLabel.setText(
+            _translate("MainWindow", "Step 3: Upload or capture!"))
         self.upload_button.setText(_translate("MainWindow", "Upload"))
         self.capture_button.setText(_translate("MainWindow", "Capture"))
         #self.pushButton.setText(_translate("MainWindow", "Submit"))
         self.exit_button.setText(_translate("MainWindow", "Exit"))
         self.stop.setText(_translate("MainWindow", "Stop"))
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
