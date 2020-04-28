d828569eaaba498c4b44dae774ebd8360f826ba7
rkabealo <rkabealo@gmail.com>
2020-04-27 22:29:36 -0400

    Modularized notification code

--- research/object_detection/inference_scripts/notification.py
+++ research/object_detection/inference_scripts/notification.py
@@ -1,73 +1,98 @@
+"""
+Created on Thu Apr 27 2020
+@author: rkabealo
+
+Class: CSE 5915 - Information Systems
+Section: 6pm TR, Spring 2020
+Prof: Prof. Jayanti
+
+A Python 3 script to perform notification of detections
+
+Usage:
+    * Called as a helper script from gui.py
+"""
+
+# Necessary imports for the sending and recieving of mail
 import smtplib, ssl
 from email.mime.text import MIMEText 
 from email.utils import formataddr  
-
 from email.mime.multipart import MIMEMultipart 
 from email.mime.base import MIMEBase 
 from email import encoders 
 
-host = "smtp.gmail.com"
-port = 587
-# User configuration
-sender_email = "tieredobjectrecognition@gmail.com"
-sender_name = "TOR SYSTEM"
-password = "tor4zeltech"
+# Other necessary imports
+import sys
 
-# Email body
-email_html = open("email.html")
-email_body = email_html.read()
+def send_notification_email(attachment): 
+    # Define a file which holds a list of users to be notified
+    reciever_file = "notified_users.txt"
 
-receiver_emails = ["rkabealo@gmail.com", "kabealo.21@buckeyemail.osu.edu"]
-default_receiver_name = "user"
+    # Define a host and port 
+    host = "smtp.gmail.com"
+    port = 587
 
-# Email body
-email_body = """
-  SEE ATTACHED.
-"""
+    # Configuration of our gmail account
+    sender_email = "tieredobjectrecognition@gmail.com"
+    sender_name = "TOR SYSTEM"
+    password = "tor4zeltech"
 
-filename = "OUTPUT"
+    # Get list of recievers from file
+    try: 
+        with open(reciever_file) as f:
+            receiver_emails = [line.rstrip() for line in f]
+    except Exception as e: 
+        print("ERROR: {}".format(e))
+        sys.exit()
 
-for receiver_email in receiver_emails:
-    print("Sending notification email...")
+    # Configure a default reciever name for people who recieve the files (since we may not have their actual names)
+    default_receiver_name = "user"
 
-    msg = MIMEMultipart()
-    msg["To"] = formataddr((default_receiver_name, receiver_email))
-    msg["From"] = formataddr((sender_name, sender_email))
-    msg["Subject"] = "Detection"
+    # Email body
+    email_body = """
+    SEE ATTACHED.
+    """
 
-    msg.attach(MIMEText(email_body, "plain"))
+    for receiver_email in receiver_emails:
+        # Display that we're attempting to send the email in the terminal 
+        print("Sending notification email...")
 
-    try:
-      with open(filename, "rb") as attachment:
-              part = MIMEBase("application", "octet-stream")
-              part.set_payload(attachment.read())
-      # Encode file in ASCII characters to send by email
-      encoders.encode_base64(part)
-      # Add header as key/value pair to attachment part
-      part.add_header(
-          "Content-Disposition",
-          f"attachment; filename= {filename}",
-      )
-      msg.attach(part)
-    except Exception as e:
-        print("ERROR: Email not sent. {} not found.".format(filename))
-        break
+        # Define the message 
+        msg = MIMEMultipart()
+        msg["To"] = formataddr((default_receiver_name, receiver_email))
+        msg["From"] = formataddr((sender_name, sender_email))
+        msg["Subject"] = "Detection"
+        msg.attach(MIMEText(email_body, "plain"))
 
-    try:
-        # Creating a SMTP session 
-        server = smtplib.SMTP(host, port)
-        # Encrypting the email
-        context = ssl.create_default_context()
-        server.starttls(context=context)
-
-        # Login to gmail account 
-        server.login(sender_email, password)
-
-        # Send the email! 
-        server.sendmail(sender_email, receiver_email, msg.as_string())
-        print("Notification email sent!")
-    except Exception as e:
-        print(e)
-    finally:
-        # Close the server
-        server.quit()
+        # Attempt to attach the file 
+        try:
+            with open(attachment, "rb") as a:
+                    part = MIMEBase("application", "octet-stream")
+                    part.set_payload(a.read())
+            # Encode file in ASCII 
+            encoders.encode_base64(part)
+            # Add header as key/value pair to attachment part
+            part.add_header(
+                "Content-Disposition",
+                f"attachment; filename= {attachment}",
+            )
+            # Attach the fully read file 
+            msg.attach(part)
+        except Exception as e:
+            print("ERROR: Email not sent. File {} not found.".format(attachment))
+            break
+        
+        # Try to send the email 
+        try:
+            server = smtplib.SMTP(host, port) # Creating a SMTP session 
+            # Encrypting the email
+            context = ssl.create_default_context() 
+            server.starttls(context=context)
+            server.login(sender_email, password) # Login to gmail account 
+            server.sendmail(sender_email, receiver_email, msg.as_string()) # Send the email! 
+            print("Notification email sent!")
+        except Exception as e:
+            # Display the exception to the user 
+            print(e)
+        finally:
+            # Close the server
+            server.quit()
